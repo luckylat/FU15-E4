82b6e42b459363f00fb2534dc10038c2735111b1
luckYrat <lacklat.key@gmail.com>
2022-07-07 11:32:56 +0900

    HoT: initial revision

---
+++ game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main(){
+    cout << "Tossing a coin..." << endl;
+    int h = 0, t = 0;
+    random_device x;
+    mt19937 mt(x());
+    for(int i = 0; 3 > i; i++){
+        cout << "Round " << i+1 << ": ";
+        if(mt()%2){
+            cout << "Heads";
+            h++;
+        }else{
+            cout << "Tails";
+            t++;
+        }
+        cout << endl;
+    }
+    cout << "Heads: " << h << ", Tails: " << t << endl;
+}
